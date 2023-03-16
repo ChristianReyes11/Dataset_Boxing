72acc581d5e534f58e00a5cc67d2e3f2c5ce0f68
ChristianReyes11 <christianamaro2000@gmail.com>
2023-03-16 10:01:40 -0600

    add filtered by age

--- boxing.py
+++ boxing.py
@@ -38,6 +38,12 @@ def filter_data_by_country(country):
     return filtered_data_country
 
 
+def filter_data_by_age(age):
+    filtered_data_age = data[data['age'].str.upper(
+    ).str.contains(age)]
+    return filtered_data_age
+
+
 data_load_state = st.text('Loading...')
 data = load_data(2500)
 data_load_state.text("")
@@ -46,6 +52,16 @@ sidebar = st.sidebar
 if st.sidebar.checkbox('Mostrar Peleadores'):
     st.subheader('Todos los peleadores')
     st.write(data)
+
+edadfighter = st.sidebar.text_input('Edad del peleador')
+btnBuscarage = st.sidebar.button('Buscar peleador')
+
+if (btnBuscarage):
+    data_fighter_age = filter_data_by_age(agefighter.upper())
+    count_row = data_fighter_age.shape[0]
+    st.write(f"Total de peleadores : {count_row}")
+    st.write(data_fighter_age)
+
 
 namefighter = st.sidebar.text_input('Nombre del peleador :')
 btnBuscar = st.sidebar.button('Buscar peleador')
